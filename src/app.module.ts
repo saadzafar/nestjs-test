4c0bd01727e8930f40ffe7f5063140f28fb2b6e6
Saad Zafar <saad@analytical.dev>
2023-10-04 22:03:08 +0500

    Use environmental config
    ~ redis config imported from env vars.

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,8 @@ import * as redisStore from 'cache-manager-redis-store';
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
     PokemonModule,
   ],
